4da6c15a4b7180ec66407cc6306ab72102dc7d5c
Mitchell Wong <mwong22@apple.com>
2019-03-22 16:10:45 -0700

    Added Author and Book models

---
+++ src/main/java/guru/springframework/spring5webapp/Controllers/AuthorController.java
@@ -0,0 +1,10 @@
+package guru.springframework.spring5webapp.Controllers;
+
+import guru.springframework.spring5webapp.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class AuthorController {
+
+    AuthorRepository authorRepository;
+}
